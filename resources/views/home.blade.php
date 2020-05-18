0a1bb3bd56f76b7007559ad0e8ef9dd0bdf4ef78
msrd1412 <bojongpesantren14@gmail.com>
2020-05-18 23:47:58 +0700

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Wilujeung Sumping^_^</h1>
+        <h3>"semoga kalian sehat selalu, dilancarkan rezekinya dan dipanjangkan umurnya aamiin"<h3>
+        
+      </div>
+    </div>
+  
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Tentang Aplikasi ini</h2>
+          <p>Aplikasi Pengaduan Masyarakat adalah sebuah aplikasi yang memudahkan masyarakat untuk membantu dalam mengatasi masalah yang di alami dengan mengadukannya menggunakan aplikasi online ini.</p>
+        </div>
+        <div class="col-md-4">
+          <h2>Tentang Pembuat</h2>
+          <p>Seorang siswa yang lulus melalui jalur virus.</p>
+        </div>
+        <div class="col-md-4">
+          <h2>Saran dari si pembuat </h2>
+          <p>Gunakannlah Aplikasi ini dengan bijak.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+  
+      <hr>
+  
+    </div> <!-- /container -->
+  
+  </main>
+  
+  <footer class="container">
+    <p align="center">&copy; Since 2019-2020 Saputra Reserved</p>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+@endsection
